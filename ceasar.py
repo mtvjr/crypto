6fddf5ee97eb57b953952a397f76c5d7356357c1
Michael Volling <mtv27@txstate.edu>
2018-03-27 20:50:17 -0500

    Version 0.1. Working Ceasar Cypher with crack

---
+++ ceasar.py
@@ -0,0 +1,57 @@
+import argparse
+
+
+def getkey():
+    key = input("Enter the shift amount (0 - 25): ")
+
+def encode(message, key):
+    ciphertext = str()
+    for char in message.lower():
+        nc = chr((ord(char) - ord('a') + key) % 26 + ord('a'))
+        ciphertext += nc
+    return ciphertext
+
+def getdefaultdict(minlen = 4):
+    dictionary = list()
+    with open("words.txt") as f:
+        for line in f.readlines():
+            if (len(line) > minlen):
+                dictionary.append(line[:-1])
+    return dictionary 
+
+def decode(cyphertext, key):
+    text = str()
+    for char in cyphertext:
+        nc = chr((ord(char) - ord('a') - key + 26) % 26 + ord('a'))
+        text += nc
+    return text
+
+def crack(cyphertext, dictionary = getdefaultdict()):
+    decoded = list()
+    solutions = list()
+    for i in range(0, 26):
+        text = decode(cyphertext, i)
+        decoded.append((i, text))
+        for word in dictionary:
+            if word in text:
+                solutions.append((i, text))
+                break
+    if (len(solutions) != 0):
+        return solutions
+    else:
+        return decoded
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Simple Ceasar Cypher")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-e', '--encode', nargs=2, metavar=('message', 'key'))
+    group.add_argument('-d', '--decode', nargs=2, metavar=('cyphertext', 'key'))
+    group.add_argument('-c', '--crack', nargs=1, metavar='cyphertext')
+    parser.add_argument('--min-word-size', type=int, default=4, metavar='num')
+    args = parser.parse_args()
+    if (args.encode is not None):
+        print(encode(args.encode[0], int(args.encode[1])))
+    elif (args.decode is not None):
+        print(decode(args.decode[0], int(args.decode[1])))
+    elif (args.crack is not None):
+        print(crack(args.crack[0], getdefaultdict(args.min_word_size)))
